39d31173c03ad414c998f3dfc48d42f27df681f7
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2019-12-26 17:26:30 -0500

    ciclos de vida de un componente

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges,  SimpleChanges } from '@angular/core';
+import { Component,
+   OnInit,
+   Input,
+   Output,
+   EventEmitter,
+   OnChanges,
+    SimpleChanges,
+   DoCheck,
+   OnDestroy 
+  } from '@angular/core';
 import { Product } from 'src/app/interfaces/product.module';
 
 @Component({
@@ -6,7 +15,7 @@ import { Product } from 'src/app/interfaces/product.module';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit, OnChanges{
+export class ProductComponent implements OnInit, DoCheck, OnDestroy{
 
   @Input() product: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
@@ -15,14 +24,27 @@ export class ProductComponent implements OnInit, OnChanges{
     console.log('1. constructor')
   }
 
+  /*
   ngOnChanges(changes: SimpleChanges) {
     console.log('2. ngOnChanges')
     console.log('changes',changes)
     
   }
 
+  */
+
   ngOnInit() {
     console.log('3. ngOnInit')
+  }
+
+  ngDoCheck() {
+    console.log('4. ngDoCheck')
+    
+  }
+
+  ngOnDestroy(){
+    console.log('5. ngOnDestroy')
+    
   }
 
 
